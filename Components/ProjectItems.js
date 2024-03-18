8ecf70c8b983c356b967e75a44f5cc0191b82720
himanshu <himanshujangir5087@gmail.com>
2024-03-18 19:12:54 +0530

    remove space

--- Components/ProjectItems.js
+++ Components/ProjectItems.js
@@ -10,7 +10,7 @@ const ProjectItems = ({ title, bgImage, ProjectUrl }) => {
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
       <Image
         className="rounded-xl group-hover:opacity-10"
-        src={`${isDevelopment ? imgBasePath : ""} ${bgImage}`}
+        src={`${isDevelopment ? imgBasePath : ""}${bgImage}`}
         height={10}
         width={600}
         alt={ProjectUrl}
